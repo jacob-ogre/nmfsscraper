4659ef631c524f45ed323c7e4f72d9e79df0f98a
Jacob Malcom <jacob.w.malcom@gmail.com>
2016-11-26 07:29:56 -0500

    Add function to get tables of ESA policy documents, update site.

--- R/get_policies.R
+++ R/get_policies.R
@@ -4,6 +4,46 @@ get_policies_page <- function() {
   page <- xml2::read_html("http://www.nmfs.noaa.gov/pr/laws/esa/policies.htm")
 }
 
+#' Get tables of NMFS ESA policies
+#'
+#' @details NMFS provides tables with document titles, dates, and citations
+#' for their ESA policy documents. This function may be particularly useful in
+#' a script that is run as a cron job to check for updates.
+#'
+#' @note This function may be fragile: it is 'tuned' to the table layout of
+#' the ESA policies page as of 26 Nov 2016. Check the policies page,
+#' \href{http://www.nmfs.noaa.gov/pr/laws/esa/policies.htm}{here}, for a layout
+#' other than one two-column table at top, followed by >1 three-column tables
+#' below, if this function fails.
+#'
+#' @return A list of two data.frames, \describe{
+#'   \item{interagency}{Table of interagency cooperation ESA policies}
+#'   \item{other_docs}{Table of other ESA policies}
+#' }
+#' @importFrom rvest html_table
+#' @importFrom dplyr bind_rows filter
+#' @export
+#' @examples
+#' \dontrun{
+#'    policies_tab <- get_policies_tables()
+#' }
+get_policies_tables <- function() {
+  page <- get_policies_page()
+  tabs <- rvest::html_table(page)
+  interagency <- tabs[[1]]
+  tabs[[1]] <- NULL
+  names(interagency) <- gsub(interagency[2, ], pattern = "/", replacement = "_")
+  interagency <- interagency[-c(1,2), ]
+  tabs <- lapply(tabs, FUN = function(x) {
+    names(x) <- gsub(x[2, ], pattern = "/", replacement = "_")
+    x <- x[-c(1,2), ]
+    x <- dplyr::filter(x, !grepl(x[[1]], pattern = "^Establishment of NOAA"))
+  })
+  other_docs <- dplyr::bind_rows(tabs)
+  return(list(interagency = interagency,
+              other_docs = other_docs))
+}
+
 #' Return PDF urls linked directly to NMFS's main policies page
 #' @export
 get_policies_pdf_urls <- function() {
